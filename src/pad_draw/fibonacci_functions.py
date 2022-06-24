516dc4ce959b280d680f097e263da23b04d77453
szabogtamas <szabogtamas@gmail.com>
2022-06-24 15:16:15 +0200

    Added some more experimental spirals

--- src/pad_draw/fibonacci_functions.py
+++ src/pad_draw/fibonacci_functions.py
@@ -86,3 +86,28 @@ def draw_fibonacci_antespiral(start, N, angle=90, ax=None):
     ax.plot([0, 12],[-0.5, -0.5], color="white")
     
     return ax
+
+def draw_fibonacci_stripe(start, N, width, angle=90, start_angle=0, ax=None):
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(3.6, 4.8))
+    
+    origin = np.array([0, 0])
+    xy = np.array([0, 0])
+    fs = fib(start, N)
+    for i, n in enumerate(fs[::-1]):
+        f2 = fs[N-i-2]
+        a2 = start_angle + angle
+        p = mpl_patches.Wedge(xy, n, start_angle, a2, color="grey", fill=True, alpha=0.3)
+        #p = mpl_patches.Wedge(xy*-1, n, start_angle, a2, color="grey", fill=True, alpha=0.3)
+        ax.add_patch(p)
+        #p = mpl_patches.Wedge(xy*-1, n-width, start_angle, a2, color="white", fill=True, alpha=1)
+        p = mpl_patches.Wedge(xy, n-width, start_angle, a2, color="white", fill=True, alpha=1)
+        ax.add_patch(p)
+        start_angle = start_angle + angle
+        slide_vector = rotate_edge((n -f2, 0), origin, start_angle)
+        xy = xy + slide_vector
+    
+    ax.set_aspect("equal", adjustable="datalim")
+    ax.axis('off')
+    ax.plot([0, 12],[-0.5, -0.5], color="white")
+    return ax
